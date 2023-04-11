cbf7731909d7c957f879220dedf26024cde3fb2e
AlexFedus <alex.fedus@gmail.com>
2023-04-11 10:04:39 -0400

    Playlist Generation Now Working

--- views.py
+++ views.py
@@ -41,8 +41,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     spotify_id = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    access_token = db.Column(db.String(200))
-    refresh_token = db.Column(db.String(200))
+    access_token = db.Column(db.String(355))
+    refresh_token = db.Column(db.String(355))
     token_expiration = db.Column(db.DateTime)
     display_name = db.Column(db.String(100))
 
